3262fe5a27a2657a8f26b312c47c9aa31610b67a
andreych <cad.dsw.dwa@gmail.com>
2024-06-15 22:16:11 +0500

    add main-page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { JobApplicationComponent } from './job-application/job-application.component';
 import { JobListComponent } from './job-list/job-list.component';
+import { MainComponent } from './main/main.component';
 
 export const routes: Routes = [
     {path: 'job-application', component: JobApplicationComponent},
-    {path: '', component: JobListComponent},
+    {path: 'job-list', component: JobListComponent},
+    {path: '' , component: MainComponent}
 ];
