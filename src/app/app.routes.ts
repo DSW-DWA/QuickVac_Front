f7a1089fc37d7a0e3e3c0b96dc74d1ad794868e7
andreych <cad.dsw.dwa@gmail.com>
2024-06-15 11:29:42 +0500

    add some basic stuff

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { JobApplicationComponent } from './job-application/job-application.component';
+import { JobListComponent } from './job-list/job-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'job-application', component: JobApplicationComponent},
+    {path: '', component: JobListComponent},
+];
